8865b1613c4ef65d315e3ad9e8fd67d558e41253
nandolopez7 <hernando.rincon@correounivalle.edu.co>
2023-05-22 23:02:58 -0500

    Diseño Login, Registro y conexion entre Login, Registro e inicio.

--- china_frontend/src/components/login_china.jsx
+++ china_frontend/src/components/login_china.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.css";
+import "../scss/login_china_style.css";
 //import 'bootstrap/dist/css/bootstrap'
-import china_templo from '../images/login_china.jpg'; // Ruta relativa de la imagen
+import china_templo from "../images/login_china.jpg"; // Ruta relativa de la imagen
 
 const getStoredUsername = () => {
   return sessionStorage.getItem("username");
 };
-
 
 export const Formulario = () => {
   const [username, setUsername] = useState("");
@@ -41,60 +41,78 @@ export const Formulario = () => {
       });
   };
 
-
   return (
-    <section className="vh-100">
-      <div className="container-fluid">
+    <div className="padre">
+      <div className="container">
         <div className="row">
-          <div className="col-sm-6 text-black">
-  
-            <div className="px-5 ms-xl-4">
-              <i className="fas fa-crow fa-2x me-3 pt-5 mt-xl-4" style={{ color: "#709085" }}></i>
-              <span className="h1 fw-bold mb-0">寻找中国</span>
-            </div>
-  
-            <div className="d-flex align-items-center h-custom-2 px-5 ms-xl-4 mt-5 pt-5 pt-xl-0 mt-xl-n5">
-  
-              <form style={{ width: "23rem" }} onSubmit={handleSubmit}>
-  
-                <h3 className="fw-normal mb-3 pb-3" style={{ letterSpacing: "1px" }}>Finding china</h3>
-  
-                <div className="form-outline mb-4">
-                  <input id="form2Example18" className="form-control form-control-lg"
+          <div className="col-md-6 offset-md-3">
+            <h1 className="text-center fw-bold mb-0">Finding China</h1>
+            <h2 className="text-center fw-bold mb-0">寻找中国</h2>
+            <div className="card my-5">
+              <form
+                className="card-body cardbody-color p-lg-5"
+                onSubmit={handleSubmit}
+              >
+                <div className="text-center">
+                  <img
+                    src="./src/images/login_logo.jpg"
+                    className="img-fluid profile-image-pic img-thumbnail rounded-circle my-3"
+                    width="200px"
+                    alt="profile"
+                  />
+                </div>
+
+                <div className="mb-3">
+                  <input
+                    id="form2Example18"
+                    className="form-control form-control-lg"
                     type="text"
                     value={username}
-                    onChange={handleUsernameChange} />
-                  <label className="form-label" htmlFor="form2Example18">Username</label>
+                    onChange={handleUsernameChange}
+                    placeholder="Username"
+                  />
                 </div>
-  
-                <div className="form-outline mb-4">
-                  <input type="password" id="form2Example28" className="form-control form-control-lg" value={password}
-                    onChange={handlePasswordChange} />
-                  <label className="form-label" htmlFor="form2Example28">Password</label>
+                <div className="mb-3">
+                  <input
+                    type="password"
+                    id="form2Example28"
+                    className="form-control form-control-lg"
+                    value={password}
+                    onChange={handlePasswordChange}
+                    placeholder="Password"
+                  />
                 </div>
-  
-                <div className="pt-1 mb-4">
-                <button className="btn btn-lg btn-block" type="submit" style={{ backgroundColor: '#FFCCCC', borderColor: '#FFCCCC' }}>Login</button>
+                <div className="text-center">
+                  <button className="btn px-5 mb-5 w-100" type="submit">
+                    Login
+                  </button>
+                </div>
+                <div
 
+                  className="form-text text-center text-dark"
+                >
+                  No estas registrado?{" "}
+                  <a className="registrarse" href="http://localhost:5173/register">
+                    {" "}
+                    Crea una cuenta
+                  </a>
                 </div>
-  
-                <p className="small mb-5 pb-lg-2"><a className="text-muted" href="#!">Forgot password?</a></p>
-                <p>Don't have an account? <a href="#!" className="link-info">Register here</a></p>
-  
+                <div
+                  className="form-text text-center text-dark"
+                >
+                  Vuelta al menú?{" "}
+                  <a className="registrarse" href="http://localhost:5173/inicio">
+                    {" "}
+                    Click aquí
+                  </a>
+                </div>
               </form>
-  
             </div>
-  
-          </div>
-          <div className="col-sm-6 px-0 d-none d-sm-block">
-            <img  src={china_templo} alt="Login image" className="w-100 vh-100" style={{ objectFit: "cover", objectPosition: "left" }} />
           </div>
         </div>
       </div>
-    </section>
+    </div>
   );
-  
-  
 };
 
-
+("");
--- china_frontend/src/components/register_form.jsx
+++ china_frontend/src/components/register_form.jsx
@@ -1,50 +1,144 @@
-import {useForm} from 'react-hook-form'
-import {createuser} from '../api/register_api'
-import { useNavigate } from 'react-router-dom'
-
-
+import { useForm } from "react-hook-form";
+import { createuser } from "../api/register_api";
+import { useNavigate } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import React, { useState } from "react";
+import { Button, Modal } from "react-bootstrap";
 
 export function Register_form() {
+  const { register, handleSubmit, watch } = useForm();
+  const navigate = useNavigate();
+  const onSubmit = handleSubmit(async (data) => {
+    await createuser(data);
+    navigate("/login");
+  });
 
-  const {register, handleSubmit} = useForm()
-  const navigate = useNavigate()
-  const onSubmit =handleSubmit( async data=>{
+  const [showModal, setShowModal] = useState(false);
 
-    await createuser(data)
-    navigate("/inicio")
-  })
+  const handleModalOpen = () => {
+    setShowModal(true);
+  };
+
+  const handleModalClose = () => {
+    setShowModal(false);
+  };
+
+  // Obtener los campos observados para la validación
+  const username = watch("username");
+  const email = watch("email");
+  const password = watch("password");
+
+  // Verificar si todos los campos requeridos están completos
+  const isFormComplete = !!username && !!email && !!password;
+
   return (
-    <form onSubmit={onSubmit} >
-      <div>
-        <label htmlFor="username">Nombre de usuario:</label>
-        <input
-          type="text"
-          id="username"
-          placeholder='Ingresa username'
-         {...register("username", {required: true})}
-        />
-      </div>
-      <div>
-        <label htmlFor="email">Correo electrónico:</label>
-        <input
-          type="email"
-          id="email"
-          placeholder='Ingresa Email'
-          {...register("email", {required: true})}
+    <div className="padre">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6 offset-md-3">
+            <h1 className="text-center fw-bold mb-0">Finding China</h1>
+            <h2 className="text-center fw-bold mb-0">寻找中国</h2>
+            <div className="card my-5">
+              <form
+                className="card-body cardbody-color p-lg-5"
+                onSubmit={onSubmit}
+              >
+                <div className="text-center">
+                  <img
+                    src="./src/images/login_logo.jpg"
+                    className="img-fluid profile-image-pic img-thumbnail rounded-circle my-3"
+                    width="200px"
+                    alt="profile"
+                  />
+                </div>
 
-        />
+                <div className="mb-3">
+                  <label htmlFor="username">Nombre de usuario:</label>
+                  <input
+                    type="text"
+                    className="form-control form-control-lg"
+                    id="username"
+                    placeholder="Ingresa username"
+                    {...register("username", { required: true })}
+                  />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="email">Correo electrónico:</label>
+                  <input
+                    type="email"
+                    className="form-control form-control-lg"
+                    id="email"
+                    placeholder="Ingresa Email"
+                    {...register("email", { required: true })}
+                  />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="password">Contraseña:</label>
+                  <input
+                    type="password"
+                    className="form-control form-control-lg"
+                    id="password"
+                    placeholder="Ingresa contraseña"
+                    {...register("password", { required: true })}
+                  />
+                </div>
+                <div className="text-center">
+                  <Button
+                    type="button"
+                    className="btn px-5 mb-5 w-100"
+                    onClick={handleModalOpen}
+                    disabled={!isFormComplete}
+                  >
+                    Enviar
+                  </Button>
+
+                  <Modal show={showModal} onHide={handleModalClose} centered backdrop="static">
+                    <Modal.Header>
+                      <Modal.Title>Registro existoso</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>¡Bienvenid@! Te has registrado con exito</Modal.Body>
+                    <Modal.Footer>
+                      <Button type="submit" variant="secondary" onClick={() => { handleModalClose(); onSubmit(); }}>
+                        Guardar
+                      </Button>
+                    </Modal.Footer>
+                  </Modal>
+                </div>
+                <div className="form-text text-center text-dark">
+                  Ya tienes cuenta?{" "}
+                  <a className="registrarse" href="http://localhost:5173/login">
+                    {" "}
+                    Inicia sesión
+                  </a>
+                </div>
+                <div className="form-text text-center text-dark">
+                  Vuelta al menú?{" "}
+                  <a
+                    className="registrarse"
+                    href="http://localhost:5173/inicio"
+                  >
+                    {" "}
+                    Click aquí
+                  </a>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    /*     <form  >
+      <div>
+
       </div>
       <div>
-        <label htmlFor="password">Contraseña:</label>
-        <input
-          type="password"
-          id="password"
-          placeholder='Ingresa contraseña'
-          {...register("password", {required: true})}
-        />
+
       </div>
-      <button type="submit">Enviar</button>
-    </form>
-  )
+      <div>
+
+      </div>
+
+    </form> */
+  );
 }
-
